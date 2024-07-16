f497290e4bb6c07b7e474a09e8044f55ca3083d4
Navdeep Ruhil <navdeep@scriza.in>
2024-07-16 12:41:55 +0530

    cors

--- src/project_setup/Express.mjs
+++ src/project_setup/Express.mjs
@@ -11,7 +11,7 @@ export default async function setupExpressApp() {
     app.use(express.urlencoded({ extended: true, limit: '1mb' }));
     app.use(cors({
         credentials: true,
-        origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:5173', 'http://localhost:62326/']
+        origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:5173', 'http://localhost:62326']
     }));
 
     app.use(express.static('src/public'));
