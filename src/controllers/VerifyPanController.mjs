bc572149ba21cacc22047b6b8f305bea73c1644e
Navdeep Ruhil <navdeep@scriza.in>
2024-07-17 17:56:51 +0530

    zoop

--- src/controllers/VerifyPanController.mjs
+++ src/controllers/VerifyPanController.mjs
@@ -1,6 +1,6 @@
 import ApiPartiesRepository from '../repositories/ApiPartiesRepository.mjs';
 import WalletRepository from '../repositories/WalletRepository.mjs';
-import { verifyPanNumberByZoop } from '../services/ZoopService.mjs'
+import { verifyPanNumberByZoop } from '../services/ZoopServicesss.mjs'
 import { CommonHandler, ValidationError, NotFoundError } from './CommonHandler.mjs';
 
 
