8b0b68618ba0330857f5f1df8c317d7208537085
Navdeep Ruhil <navdeep@scriza.in>
2024-07-17 18:24:58 +0530

    category

--- src/models/ServiceTableModel.mjs
+++ src/models/ServiceTableModel.mjs
@@ -4,7 +4,7 @@ import { Schema, model } from 'mongoose';
 const serviceTableSchema = new Schema({
     serviceName: { type: String, require: true, trim: true, uppercase: true },
     serviceId: { type: Number, require: true },
-    category: { type: String, require: true },
+    category: { type: String, require: true, trim: true, uppercase: true },
     image: { type: String, require: true, default: 'NaN'}
 }, {
     timestamps: true
