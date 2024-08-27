367644a715d093aaabd2c9be1a226787dfa52a6e
Navdeep Ruhil <navdeep@scriza.in>
2024-08-27 11:56:46 +0530

    success

--- src/controllers/VerifyController.mjs
+++ src/controllers/VerifyController.mjs
@@ -58,7 +58,7 @@ class VerifyController {
             
             const result = await verifyByOperator(documentDetails);
             const responseMessage = result.response_message || result.data?.response_message || 'Unknown error occurred during verification';
-            await TransactionHistoryRepository.updateTransactionHistoryById(transaction.id, { endResult: `Api response: ${responseMessage}, Api result: ${await CommonHandler.safeStringify(result)}`, status: 'Complete' });
+            await TransactionHistoryRepository.updateTransactionHistoryById(transaction.id, { endResult: `Api response: ${responseMessage}, Api result: ${await CommonHandler.safeStringify(result)}`, status: 'Success' });
 
             // Result handling
             if (result.response_message === 'Valid Authentication') { res.status(200).json({ status: 200, success: true, message: `User ${serviceType} details fetched successfully`, data: result }); }
