4457ccd9ee585fb90247d71d2e0002291ffb487e
Navdeep Ruhil <navdeep@scriza.in>
2024-09-17 12:48:22 +0530

    createPackage

--- src/controllers/PackageSetupController.mjs
+++ src/controllers/PackageSetupController.mjs
@@ -6,6 +6,7 @@ class PackageSetupController{
     static async createPackageSetup(req, res) {
         try {
             const packageSetupData = await PackageSetupController.validatePackageSetupData(req.body);
+            console.log(packageSetupData);
             const packageSetup = await PackageSetupRepository.createPackageSetup(packageSetupData);
             res.status(201).json({ status: 201, success: true, message: 'Package setup created successfully', data: packageSetup });
         } catch (error) {
@@ -29,13 +30,12 @@ class PackageSetupController{
 
     static async validatePackageSetupData(data) {
         const { packageName, servicesProvided } = data;
-        console.log(" Services Provided ",  servicesProvided);
 
         if (!packageName) { throw new ValidationError('Package name is required'); }
         if (!servicesProvided || !Array.isArray(servicesProvided) || servicesProvided.length === 0) { throw new ValidationError('At least one service must be provided'); }
 
-        const existingPackage = await PackageSetupRepository.getPackageSetupByPackageName('PLATNIUM PACKAGE');
-        console.log("Existing Package", existingPackage);
+        const existingPackage = await PackageSetupRepository.getPackageSetupByPackageName('BASE PACKAGE');
+        if (!existingPackage) { throw new Error('Package not found'); }
 
         for (const service of servicesProvided) {
             if (!service.serviceType) { throw new ValidationError('Service type is required'); }
@@ -43,6 +43,15 @@ class PackageSetupController{
             if (service.serviceLimit == null) { throw new ValidationError('Service limit is required'); }
             if (service.serviceLifeSpan == null) { throw new ValidationError('Service life span is required'); }
         }
+
+        const updatedServicesProvided = servicesProvided.map(service => {
+            const matchingService = existingPackage.servicesProvided.find( existingService => existingService.serviceType === service.serviceType );
+
+            if (matchingService) { return { ...service, serviceUrl: matchingService.serviceUrl  };
+            } else { return { ...service, serviceUrl: 'URL not found' }; } });
+
+        data.servicesProvided = updatedServicesProvided;
+
         return data;
     }
 }
